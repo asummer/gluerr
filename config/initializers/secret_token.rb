77127e13144811168151304bbfe02d1d276a7cda
Aaron Summer <asummer@purolator.com>
2012-10-31 10:43:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gluerr::Application.config.secret_token = '1455d10e5308a5d1324f0cb5ee5bc44b0691d7efa90495bd8c379e30324373a48013ca3ffaa9eebbd132f34718f24908b44d70a153f8456ffdb275fbaae3e005'
